1e6972cddb198fc005521e2fefaa41f8b5c39026
Cameron <w0796224@myscc.ca>
2024-12-19 19:04:13 -0500

    Concept changes * Added Work Section * Changed footer * Fixed accordion styling

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -7,37 +7,39 @@ import Nav from "react-bootstrap/Nav";
 export default function Footer() {
     return (
         <footer className="footer">
-            <Container fluid>
-                <Row className= "bg-primary text-white p-4">
-                    <Col className="mx-5">
-                        <Stack>
-                            <Image
-                                src="logo192.png"
-                                alt="Logo"
-                                rounded
-                                width={150}
-                                height={150}
-                            />
-                            <h2>Cameron McRae</h2>
-                            <p>Web Developer</p>
-                        </Stack>
-                    </Col>
-                    <Col>
-                        <Nav className="flex-column fs-5">
-                            <p>Useful Links</p>
-                            <NavLink href="#about" className="text-white">About</NavLink>
-                            <NavLink href="#projects" className="text-white">Projects</NavLink>
-                            <NavLink href="#skills" className="text-white">Skills</NavLink>
-                            <NavLink href="#contact" className="text-white">Contact</NavLink>
-                        </Nav>
-                    </Col>
-                    <Col>
-                        <h4>Contact Me</h4>
-                        <p>email: w0796224@myscc.ca</p>
-                        <p>phone: +1(226)350-3953</p>
-                    </Col>
-                </Row>
-            </Container>
+            {/*<Container fluid>*/}
+            {/*    <Row className= "bg-primary text-white p-4">*/}
+            {/*        <Col className="mx-5">*/}
+            {/*            <Stack>*/}
+            {/*                <Image*/}
+            {/*                    src="logo192.png"*/}
+            {/*                    alt="Logo"*/}
+            {/*                    rounded*/}
+            {/*                    width={150}*/}
+            {/*                    height={150}*/}
+            {/*                />*/}
+            {/*                <h2>Cameron McRae</h2>*/}
+            {/*                <p>Web Developer</p>*/}
+            {/*            </Stack>*/}
+            {/*        </Col>*/}
+            {/*        <Col>*/}
+            {/*            <Nav className="flex-column fs-5">*/}
+            {/*                <p>Useful Links</p>*/}
+            {/*                <NavLink href="#about" className="text-white">About</NavLink>*/}
+            {/*                <NavLink href="#projects" className="text-white">Projects</NavLink>*/}
+            {/*                <NavLink href="#skills" className="text-white">Skills</NavLink>*/}
+            {/*                <NavLink href="#contact" className="text-white">Contact</NavLink>*/}
+            {/*            </Nav>*/}
+            {/*        </Col>*/}
+            {/*        <Col>*/}
+            {/*            <h4>Contact Me</h4>*/}
+            {/*            <p>email: w0796224@myscc.ca</p>*/}
+            {/*            <p>phone: +1(226)350-3953</p>*/}
+            {/*        </Col>*/}
+            {/*    </Row>*/}
+            {/*</Container>*/}
+
+            <p className="footer-text">Loosely designed in Figma and coded in Visual Studio Code by yours truly. Built with Next.js and Tailwind CSS, deployed with Vercel. All text is set in the Inter typeface.</p>
         </footer>
     )
 }
--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -7,17 +7,37 @@ export default function work() {
             <h1>Work</h1>
             <Card>
                 <Card.Body>
-
+                    <div className="card-body-left">
+                        <p className="date-text">2024 - present</p>
+                    </div>
+                    <div className="card-body-right">
+                        <h3 className="body-text">Family Respite Services</h3>
+                        <p className="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At commodi
+                            eaque fugit inventore iusto, nulla quasi vero. Dolor dolorem expedita iste quaerat quis.
+                            Amet atque corporis doloremque harum repellendus! Architecto!</p>
+                    </div>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Body>
-
+                    <div className="card-body-left">
+                        <p className="date-text">2024 - present</p>
+                    </div>
+                    <div className="card-body-right">
+                        <h3 className="body-text">Family Respite Services</h3>
+                        <p className="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At commodi eaque fugit inventore iusto, nulla quasi vero. Dolor dolorem expedita iste quaerat quis. Amet atque corporis doloremque harum repellendus! Architecto!</p>
+                    </div>
                 </Card.Body>
             </Card>
             <Card>
                 <Card.Body>
-
+                    <div className="card-body-left">
+                        <p className="date-text">2024 - present</p>
+                    </div>
+                    <div className="card-body-right">
+                        <h3 className="body-text">Family Respite Services</h3>
+                        <p className="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At commodi eaque fugit inventore iusto, nulla quasi vero. Dolor dolorem expedita iste quaerat quis. Amet atque corporis doloremque harum repellendus! Architecto!</p>
+                    </div>
                 </Card.Body>
             </Card>
         </div>
